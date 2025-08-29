a7ba9c42f18ad8398a9e02b511568493007537f6
StaeyKay <ekwashie001@st.ug.edu.gh>
2025-08-29 09:34:36 +0000

    added user routes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import connectDb from './config/db.js';
+import UserRouter from './routes/user.js'
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use('/api/auth', UserRouter);
 
 app.listen(PORT, () => {
     connectDb();
