c2526a47376928fdf1b07e9c054bc98be3a5ac7f
StaeyKay <ekwashie001@st.ug.edu.gh>
2025-08-29 09:53:11 +0000

    login works

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signUp } from "../controllers/user.js";
+import { login, signUp } from "../controllers/user.js";
 
 const router = Router();
 
 router.post('/signup', signUp);
+router.post('/login', login);
 
 export default router;
